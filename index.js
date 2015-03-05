c1f0b19149b42d9c43c1263552c283df1110f0ec
Ryan Hayes <ryan.hayes@pcdhc.com>
2015-03-05 14:11:26 -0500

    Updated package names/versions.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var checker   = require('ember-cli-version-checker');
 
 module.exports = {
-  name: 'ember-cli-babel',
+  name: 'ember-cli-typescript',
 
   shouldSetupRegistryInIncluded: function() {
     return !checker.isAbove(this, '0.2.0');
@@ -11,8 +11,8 @@ module.exports = {
     var options = getOptions(this.parent && this.parent.options && this.parent.options['babel']);
 
     var plugin = {
-      name: 'ember-cli-babel',
-      ext: 'js',
+      name: 'ember-cli-typescript',
+      ext: 'ts',
       toTree: function(tree) {
         return require('broccoli-babel-transpiler')(tree, options);
       }
@@ -32,24 +32,4 @@ module.exports = {
 
 function getOptions(options) {
   options = options || {};
-
-  // Ensure modules aren't compiled unless explicitly set to compile
-  options.blacklist = options.blacklist || ['es6.modules'];
-
-  if (options.compileModules === true) {
-    if (options.blacklist.indexOf('es6.modules') >= 0) {
-      options.blacklist.splice(options.blacklist.indexOf('es6.modules'), 1);
-    }
-    
-    delete options.compileModules;
-  } else {
-    if (options.blacklist.indexOf('es6.modules') < 0) {
-      options.blacklist.push('es6.modules');
-    }
-  }
-
-  // Ember-CLI inserts its own 'use strict' directive
-  options.blacklist.push('useStrict');
-
-  return options;
 }
